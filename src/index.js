467ae1fccda21256dac1b4feb6c1afa71523bba8
Artur <artur.georgizov1@gmail.com>
2017-09-26 07:09:42 -0700

    all test passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let flag = true;
+    let stack = [str[0]];
+
+    for(let i = 1; i < str.length; i++){
+        for(let arr of bracketsConfig){
+            if(str[i] == arr[0] && arr[0] == arr[1]){
+                if(stack.length == 0)
+                    stack.push(str[i]);
+                else if(stack[stack.length -1] == arr[0])
+                    stack.pop();
+                else
+                    stack.push(str[i]);
+            }
+            else if(str[i] == arr[0])
+                stack.push(str[i]);
+            else if(str[i] == arr[1]){
+                if(stack.length == 0)
+                    flag = false;
+                else if(stack[stack.length-1] == arr[0])
+                    stack.pop();
+                else
+                    flag = false;
+            }
+        }
+    }
+    if(stack.length!=0)
+        flag = false;
+    return flag;
 }
+
